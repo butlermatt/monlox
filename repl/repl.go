ab86af5dd2f2e24439e4c6311a2e3bebc785e847
Matthew Butler <butler.matthew@gmail.com>
2018-04-02 21:54:29 -0300

    Make repl run evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/butlermatt/monlox/evaluator"
 	"github.com/butlermatt/monlox/lexer"
 	"github.com/butlermatt/monlox/parser"
 )
@@ -32,8 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
